f45589866970dcbd4b2dc3ab04d19211b45cfd86
Yauheni <lynxfortest@gmail.com>
2021-02-25 11:37:50 +0300

    first

--- src/by/metelski/compositechain/entity/impl/Composite.java
+++ src/by/metelski/compositechain/entity/impl/Composite.java
@@ -6,25 +6,25 @@ public class Composite implements Component {
 
 	@Override
 	public void operation() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void add(Component c) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void remove(Component c) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public Object getChild(int index) {
-		// TODO Auto-generated method stub
+	
 		return null;
 	}
 
